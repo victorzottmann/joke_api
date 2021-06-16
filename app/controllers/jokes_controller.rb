ed7676bd49501d777cc87eeacb06f2bd8fe2714f
victorzottmann <zottmann.victor@gmail.com>
2021-06-17 00:39:37 +1000

    add joke ownership verification

--- app/controllers/jokes_controller.rb
+++ app/controllers/jokes_controller.rb
@@ -1,13 +1,14 @@
 class JokesController < ApplicationController
+  before_action :authenticate_user, except: [:index, :random, :show]
   before_action :set_joke, only: [:show, :update, :destroy]
-
+  before_action :check_ownership, only: [:update, :destroy]
   def index
     @jokes = Joke.all
     render json: @jokes
   end
 
   def create
-    @joke = Joke.create(joke_params)
+    @joke = current_user.jokes.create(joke_params)
     if @joke.errors.any?
       render json: @joke.errors, status: :unprocessable_entity
     else
@@ -58,6 +59,12 @@ class JokesController < ApplicationController
         @joke = Joke.find(params[:id])
       rescue
         render json: {error: "Joke not found"}, status: 404
+      end
+    end
+
+    def check_ownership
+      if current_user.id != @joke.user.id
+        render json: {error: "You don't have permission to do that"}, status: 401 # unauthorized
       end
     end
 end
