0fbdf9b06effdb5450035afcaf86135a6b6c8253
victorzottmann <zottmann.victor@gmail.com>
2021-06-17 00:09:16 +1000

    create sign in functionality in user controller and route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,21 @@
 class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
-    
     if @user.save
-      render json: @user, status: 201 # or :create (rails convention)
+      auth_token = Knock::AuthToken.new payload: {sub: @user.id}
+      render json: {username: @user.username, jwt: auth_token.token}, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def sign_in
+    @user = User.find_by_email(params[:email])
+    if @user && @user.authenticate(params[:password])
+      auth_token = Knock::AuthToken.new payload: {sub: @user.id}
+      render json: {username: @user.username, jwt: auth_token.token}, status: 200
+    else
+      render json: {error: "Incorrect Email Address or Password"}, status: 404
     end
   end
 
