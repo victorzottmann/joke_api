5c7f83791ae0e199f7f340374f717aa33b7ae52e
victorzottmann <zottmann.victor@gmail.com>
2021-06-17 00:25:04 +1000

    fix bug with loading jokes on database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,13 @@
 
 joke_categories = ["Puns", "Programming", "Knock Knock", "Limericks"]
 
+if Category.all.length == 0
+  joke_categories.each do |category|
+    Category.create(name: category)
+    puts "created #{category} category"
+  end
+end
+
 if User.count == 0
   User.create(
     username: "Victor", 
@@ -31,10 +38,3 @@ if Joke.all.length == 0
   Joke.create(user_id: 2, category_id: 2, body: "This is a very random programming joke")
   Joke.create(user_id: 2, category_id: 2, body: "Hello there, this is a random programming joke")
 end
-
-# if Category.all.length == 0
-#   joke_categories.each do |category|
-#     Category.create(name: category)
-#     puts "created #{category} category"
-#   end
-# end
